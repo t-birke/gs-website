8686a388b580114ffa432cd3705524d5b5814b28
Christoph Hartig <christoph.hartig@gmail.com>
2014-02-18 17:23:48 +0100

    include popup images

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,7 +244,7 @@ module.exports = function (grunt) {
             'images/{,*/}*.{gif,webp}',
             'scripts/vendor/**/*',
             'styles/fonts/*',
-            'popups/*'
+            'popups/**/*'
           ]
         }]
       }
